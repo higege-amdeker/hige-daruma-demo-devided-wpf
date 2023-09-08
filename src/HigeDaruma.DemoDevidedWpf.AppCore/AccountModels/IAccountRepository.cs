4499519872fedea0526502326481f69b37eb9f17
higege-amdeker <higezozozo@gmail.com>
2023-09-08 23:52:42 +0900

    MockAccountRepository 追加

---
+++ src/HigeDaruma.DemoDevidedWpf.AppCore/AccountModels/IAccountRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace HigeDaruma.DemoDevidedWpf.AppCore.AccountModels;
+
+/// <summary>
+/// <see cref="Account"/> の永続化インターフェイス
+/// </summary>
+public interface IAccountRepository
+{
+    /// <summary>
+    /// ID を指定して <see cref="Account"/> を読みだします
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public Task<Account> ReadAsync(Guid id);
+}
---
+++ src/HigeDaruma.DemoDevidedWpf.AppCore/AccountModels/MockAccountRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace HigeDaruma.DemoDevidedWpf.AppCore.AccountModels;
+
+/// <summary>
+/// <see cref="Account"/> の永続化を実現します
+/// </summary>
+public sealed class MockAccountRepository : IAccountRepository
+{
+    /// <summary>
+    /// コンストラクター
+    /// </summary>
+    public MockAccountRepository()
+    {
+    }
+
+    /// <inheritdoc/>
+    public async Task<Account> ReadAsync(Guid id)
+    {
+        string name = "MockName";
+        Account account = new(id, name);
+
+        return await Task.FromResult(account);
+    }
+}
