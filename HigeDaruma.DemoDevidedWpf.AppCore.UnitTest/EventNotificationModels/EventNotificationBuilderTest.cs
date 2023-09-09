bb6cb8d012a0b58ce1099ad46062931e94c0862e
higege-amdeker <higezozozo@gmail.com>
2023-09-09 23:11:50 +0900

    Start/End をメソッドで設定するように

--- HigeDaruma.DemoDevidedWpf.AppCore.UnitTest/EventNotificationModels/EventNotificationBuilderTest.cs
+++ HigeDaruma.DemoDevidedWpf.AppCore.UnitTest/EventNotificationModels/EventNotificationBuilderTest.cs
@@ -17,6 +17,64 @@ public sealed class EventNotificationBuilderTest
         Assert.NotNull(result);
     }
 
+    // Start(DateTimeOffset)
+
+    [Fact]
+    public void Start_ThrowsArgumentOutOfRangeException_WhenInputStartIsMinValue()
+    {
+        static void testCode()
+        {
+            EventNotificationBuilder instance = new();
+            DateTimeOffset start = DateTimeOffset.MinValue;
+            instance.Start(start);
+        }
+
+        Assert.Throws<ArgumentOutOfRangeException>(testCode);
+    }
+
+    [Fact]
+    public void Start_ThrowsInvalidOperationException_WhenInputStartIsOkButInstanceIsAlreadySetStart()
+    {
+        static void testCode()
+        {
+            EventNotificationBuilder instance = new();
+            DateTimeOffset start = new(2023, 9, 9, 12, 0, 0, new TimeSpan(0));
+            instance.Start(start);
+            instance.Start(start);
+        }
+
+        Assert.Throws<InvalidOperationException>(testCode);
+    }
+
+    // End(DateTimeOffset)
+
+    [Fact]
+    public void End_ThrowsArgumentOutOfRangeException_WhenInputEndIsMinValue()
+    {
+        static void testCode()
+        {
+            EventNotificationBuilder instance = new();
+            DateTimeOffset end = DateTimeOffset.MinValue;
+            instance.End(end);
+        }
+
+        Assert.Throws<ArgumentOutOfRangeException>(testCode);
+    }
+
+    [Fact]
+    public void End_ThrowsInvalidOperationException_WhenInputStartIsOkButInstanceIsAlreadySetStart()
+    {
+        static void testCode()
+        {
+            EventNotificationBuilder instance = new();
+            DateTimeOffset end = new(2023, 9, 9, 12, 0, 0, new TimeSpan(0));
+            instance.End(end);
+            instance.End(end);
+        }
+
+        Assert.Throws<InvalidOperationException>(testCode);
+    }
+
     // SetName(string)
 
     [Fact]
@@ -104,15 +162,22 @@ public sealed class EventNotificationBuilderTest
     [Fact]
     public void Build_ReturnsOk_WhenInstanceIsOk()
     {
+        DateTimeOffset start = new(2023, 9, 9, 12, 0, 0, new TimeSpan(0));
+        DateTimeOffset end = new(2023, 9, 10, 12, 0, 0, new TimeSpan(0));
+        string name = "test";
+        string bandName = "test";
         EventNotificationBuilder instance = new();
-        instance.SetName("test");
-        instance.AddBandName("test");
+        instance.Start(start);
+        instance.End(end);
+        instance.SetName(name);
+        instance.AddBandName(bandName);
 
         EventNotification result = instance.Build();
 
         Assert.NotNull(result);
-        Assert.Equal("test", result.Name);
-        // TODO: 他のプロパティのテスト
+        Assert.Equal(start, result.StartDateTime);
+        Assert.Equal(end, result.EndDateTime);
+        Assert.Equal(name, result.Name);
+        Assert.Equal(bandName, result.BandNames.First());
     }
-
 }
--- src/HigeDaruma.DemoDevidedWpf.AppCore/EventNotificationModels/EventNotificationBuilder.cs
+++ src/HigeDaruma.DemoDevidedWpf.AppCore/EventNotificationModels/EventNotificationBuilder.cs
@@ -5,8 +5,8 @@
 /// </summary>
 public sealed class EventNotificationBuilder
 {
-    private readonly DateTimeOffset _startDateTime;
-    private DateTimeOffset _endDateTime;
+    private DateTimeOffset _startDateTime = DateTimeOffset.MinValue;
+    private DateTimeOffset _endDateTime = DateTimeOffset.MinValue;
     private string? _name;
     private readonly List<string> _bandNames = new();
 
@@ -15,7 +15,40 @@ public sealed class EventNotificationBuilder
     /// </summary>
     public EventNotificationBuilder()
     {
-        _startDateTime = DateTimeOffset.Now;
+    }
+
+    /// <summary>
+    /// 開始日を設定します
+    /// </summary>
+    /// <param name="start"></param>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    /// <exception cref="InvalidOperationException"></exception>
+    public void Start(DateTimeOffset start)
+    {
+        if (start == DateTimeOffset.MinValue)
+            throw new ArgumentOutOfRangeException(nameof(start), "最小値は設定できません。");
+
+        if (_startDateTime != DateTimeOffset.MinValue)
+            throw new InvalidOperationException("開始日は既に設定されています。");
+
+        _startDateTime = start;
+    }
+
+    /// <summary>
+    /// 終了日を設定します
+    /// </summary>
+    /// <param name="end"></param>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    /// <exception cref="InvalidOperationException"></exception>
+    public void End(DateTimeOffset end)
+    {
+        if (end == DateTimeOffset.MinValue)
+            throw new ArgumentOutOfRangeException(nameof(end), "最小値は設定できません。");
+
+        if (_endDateTime != DateTimeOffset.MinValue)
+            throw new InvalidOperationException("終了日は既に設定されています。");
+
+        _endDateTime = end;
     }
 
     /// <summary>
@@ -51,10 +84,14 @@ public sealed class EventNotificationBuilder
     /// <exception cref="InvalidOperationException"></exception>
     public EventNotification Build()
     {
+        if (_startDateTime == DateTimeOffset.MinValue)
+            throw new InvalidOperationException($"{nameof(_startDateTime)} が設定されていません。");
+
+        if (_endDateTime == DateTimeOffset.MinValue)
+            throw new InvalidOperationException($"{nameof(_endDateTime)} が設定されていません。");
+
         if (_name is null)
             throw new InvalidOperationException($"{nameof(_name)} が設定されていません。");
-
-        _endDateTime = DateTimeOffset.Now;
 
         EventNotification notification = new(
             _startDateTime,
