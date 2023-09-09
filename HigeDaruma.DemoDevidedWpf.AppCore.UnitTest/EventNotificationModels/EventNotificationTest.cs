48d225a56d28fe2641ec83a97ea4384b227ee907
higege-amdeker <higezozozo@gmail.com>
2023-09-09 10:19:48 +0900

    EventNotification クラスを追加

---
+++ HigeDaruma.DemoDevidedWpf.AppCore.UnitTest/EventNotificationModels/EventNotificationTest.cs
@@ -0,0 +1,31 @@
+﻿using HigeDaruma.DemoDevidedWpf.AppCore.EventNotificationModels;
+
+namespace HigeDaruma.DemoDevidedWpf.AppCore.UnitTest.EventNotificationModels;
+
+/// <summary>
+/// <see cref="Event"/>
+/// </summary>
+public sealed class EventNotificationTest
+{
+    [Fact]
+    public void Ctor_ReturnsOk_WhenInputIsOk()
+    {
+        DateTimeOffset startDateTime = DateTimeOffset.Now;
+        DateTimeOffset endDateTime = startDateTime.AddDays(1);
+        string name = "Live 2023";
+        List<string> bandNames = new()
+        {
+            "test",
+            "FixedCycles",
+        };
+
+        EventNotification result = new(startDateTime, endDateTime, name, bandNames);
+
+        Assert.NotNull(result);
+        Assert.Equal(startDateTime, result.StartDateTime);
+        Assert.Equal(endDateTime, result.EndDateTime);
+        Assert.Equal(name, result.Name);
+        Assert.Equal(bandNames, result.BandNames);
+    }
+
+}
---
+++ src/HigeDaruma.DemoDevidedWpf.AppCore/EventNotificationModels/EventNotification.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.ObjectModel;
+
+namespace HigeDaruma.DemoDevidedWpf.AppCore.EventNotificationModels;
+
+/// <summary>
+/// イベント通知を表現する DomainObject です
+/// </summary>
+public sealed class EventNotification
+{
+    /// <summary>
+    /// イベントの開始日時
+    /// </summary>
+    public DateTimeOffset StartDateTime { get; }
+
+    /// <summary>
+    /// イベントの終了日時
+    /// </summary>
+    public DateTimeOffset EndDateTime { get; }
+
+    /// <summary>
+    /// イベントの名前
+    /// </summary>
+    public string Name { get; }
+
+    /// <summary>
+    /// イベントに参加するバンド名の一覧の実態
+    /// </summary>
+    private readonly List<string> _bandNames;
+
+    /// <summary>
+    /// コンストラクター
+    /// </summary>
+    /// <param name="startDateTime"></param>
+    /// <param name="endDateTime"></param>
+    /// <param name="name"></param>
+    /// <param name="bandNames"></param>
+    internal EventNotification(
+        DateTimeOffset startDateTime,
+        DateTimeOffset endDateTime,
+        string name,
+        List<string> bandNames)
+    {
+        StartDateTime = startDateTime;
+        EndDateTime = endDateTime;
+        Name = name;
+        _bandNames = bandNames;
+    }
+
+    /// <summary>
+    /// イベントに参加するバンド名の一覧の公開用プロパティ
+    /// </summary>
+    public ReadOnlyCollection<string> BandNames => _bandNames.AsReadOnly();
+}
