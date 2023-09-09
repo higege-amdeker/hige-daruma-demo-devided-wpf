7494fb2c7d885a3d2773a65a982ab668c148fd7b
higege-amdeker <higezozozo@gmail.com>
2023-09-09 20:12:27 +0900

    EventNotificationBuilder 追加

---
+++ HigeDaruma.DemoDevidedWpf.AppCore.UnitTest/EventNotificationModels/EventNotificationBuilderTest.cs
@@ -0,0 +1,118 @@
+﻿using HigeDaruma.DemoDevidedWpf.AppCore.EventNotificationModels;
+
+namespace HigeDaruma.DemoDevidedWpf.AppCore.UnitTest.EventNotificationModels;
+
+/// <summary>
+/// <see cref="EventNotificationBuilder"/> の UnitTest です
+/// </summary>
+public sealed class EventNotificationBuilderTest
+{
+    // Ctor()
+
+    [Fact]
+    public void Ctor_ReturnsOk()
+    {
+        EventNotificationBuilder result = new();
+
+        Assert.NotNull(result);
+    }
+
+    // SetName(string)
+
+    [Fact]
+    public void SetName_ThrowsArgumentException_WhenInstanceIsOkButInputNameIsNull()
+    {
+        static void testCode()
+        {
+            EventNotificationBuilder instance = new();
+            string? name = null;
+            instance.SetName(name!);
+        }
+
+        Assert.Throws<ArgumentException>(testCode);
+    }
+
+    [Fact]
+    public void SetName_ThrowsArgumentException_WhenInstanceIsOkButInputNameIsEmpty()
+    {
+        static void testCode()
+        {
+            EventNotificationBuilder instance = new();
+            string name = string.Empty;
+            instance.SetName(name);
+        }
+
+        Assert.Throws<ArgumentException>(testCode);
+    }
+
+    [Fact]
+    public void SetName_ThrowsArgumentException_WhenInstanceIsOkButInputNameIsWhiteSpace()
+    {
+        static void testCode()
+        {
+            EventNotificationBuilder instance = new();
+            string name = " ";
+            instance.SetName(name);
+        }
+
+        Assert.Throws<ArgumentException>(testCode);
+    }
+
+    // AddBandName(string)
+
+    [Fact]
+    public void AddBandName_ThrowsArgumentException_WhenInstanceIsOkButInputNameIsNull()
+    {
+        static void testCode()
+        {
+            EventNotificationBuilder instance = new();
+            string? bandName = null;
+            instance.AddBandName(bandName!);
+        }
+
+        Assert.Throws<ArgumentException>(testCode);
+    }
+
+    [Fact]
+    public void AddBandName_ThrowsArgumentException_WhenInstanceIsOkButInputNameIsEmpty()
+    {
+        static void testCode()
+        {
+            EventNotificationBuilder instance = new();
+            string bandName = string.Empty;
+            instance.AddBandName(bandName);
+        }
+
+        Assert.Throws<ArgumentException>(testCode);
+    }
+
+    [Fact]
+    public void AddBandName_ThrowsArgumentException_WhenInstanceIsOkButInputNameIsWhiteSpace()
+    {
+        static void testCode()
+        {
+            EventNotificationBuilder instance = new();
+            string bandName = " ";
+            instance.AddBandName(bandName);
+        }
+
+        Assert.Throws<ArgumentException>(testCode);
+    }
+
+    // Build()
+
+    [Fact]
+    public void Build_ReturnsOk_WhenInstanceIsOk()
+    {
+        EventNotificationBuilder instance = new();
+        instance.SetName("test");
+        instance.AddBandName("test");
+
+        EventNotification result = instance.Build();
+
+        Assert.NotNull(result);
+        Assert.Equal("test", result.Name);
+        // TODO: 他のプロパティのテスト
+    }
+
+}
---
+++ src/HigeDaruma.DemoDevidedWpf.AppCore/EventNotificationModels/EventNotificationBuilder.cs
@@ -0,0 +1,67 @@
+﻿namespace HigeDaruma.DemoDevidedWpf.AppCore.EventNotificationModels;
+
+/// <summary>
+/// <see cref="EventNotification"/> の構築ロジックを提供します
+/// </summary>
+public sealed class EventNotificationBuilder
+{
+    private readonly DateTimeOffset _startDateTime;
+    private DateTimeOffset _endDateTime;
+    private string? _name;
+    private readonly List<string> _bandNames = new();
+
+    /// <summary>
+    /// コンストラクター
+    /// </summary>
+    public EventNotificationBuilder()
+    {
+        _startDateTime = DateTimeOffset.Now;
+    }
+
+    /// <summary>
+    /// 通知の名前を設定します
+    /// </summary>
+    /// <param name="name"></param>
+    /// <exception cref="ArgumentException"></exception>
+    public void SetName(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("null, Empty, white space ではいけません。", nameof(name));
+
+        _name = name;
+    }
+
+    /// <summary>
+    /// イベントの参加バンド名を追加します
+    /// </summary>
+    /// <param name="bandName"></param>
+    /// <exception cref="ArgumentException"></exception>
+    public void AddBandName(string bandName)
+    {
+        if (string.IsNullOrWhiteSpace(bandName))
+            throw new ArgumentException("null, Empty, white space ではいけません。", nameof(bandName));
+
+        _bandNames.Add(bandName);
+    }
+
+    /// <summary>
+    /// <see cref="EventNotification"/> を構築します
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="InvalidOperationException"></exception>
+    public EventNotification Build()
+    {
+        if (_name is null)
+            throw new InvalidOperationException($"{nameof(_name)} が設定されていません。");
+
+        _endDateTime = DateTimeOffset.Now;
+
+        EventNotification notification = new(
+            _startDateTime,
+            _endDateTime,
+            _name,
+            _bandNames);
+
+        return notification;
+    }
+}
