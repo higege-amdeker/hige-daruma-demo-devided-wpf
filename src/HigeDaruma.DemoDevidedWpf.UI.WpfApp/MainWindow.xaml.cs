ddb6dfff92d2e468d9d352a2b28270d978da54c0
higege-amdeker <higezozozo@gmail.com>
2023-09-10 00:18:16 +0900

    UI.WpfApp EventNotification の実装を追加

--- src/HigeDaruma.DemoDevidedWpf.UI.WpfApp/MainWindow.xaml.cs
+++ src/HigeDaruma.DemoDevidedWpf.UI.WpfApp/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using HigeDaruma.DemoDevidedWpf.AppCore.AccountModels;
+using HigeDaruma.DemoDevidedWpf.AppCore.EventNotificationModels;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -11,6 +13,8 @@ public partial class MainWindow : Window
 {
     private readonly IAccountRepository _accountRepository;
 
+    private readonly EventNotificationBuilder _eventNotificationBuilder = new();
+
     public MainWindow()
     {
         _accountRepository = new MockAccountRepository();
@@ -18,6 +22,9 @@ public partial class MainWindow : Window
         InitializeComponent();
 
         ContentRendered += async (s, e) => await GetAccount();
+
+        NameTextBox.Text = "test";
+        BandNameTextBox.Text = "test";
     }
 
     /// <summary>
@@ -31,5 +38,79 @@ public partial class MainWindow : Window
 
         Id.Text = account.Id.ToString();
         Name.Text = account.Name;
+    }
+
+    private void StartButton_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            DateTimeOffset now = DateTimeOffset.Now;
+
+            _eventNotificationBuilder.Start(now);
+        }
+        catch (Exception ex)
+        {
+            _eventNotificationBuilder.Clear();
+            MessageBox.Show(ex.Message);
+        }
+    }
+
+    private void SetNameButton_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            string name = NameTextBox.Text;
+
+            _eventNotificationBuilder.SetName(name);
+        }
+        catch (Exception ex)
+        {
+            _eventNotificationBuilder.Clear();
+            MessageBox.Show(ex.Message);
+        }
+    }
+
+    private void AddBandNameButton_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            string bandName = BandNameTextBox.Text;
+
+            _eventNotificationBuilder.AddBandName(bandName);
+        }
+        catch (Exception ex)
+        {
+            _eventNotificationBuilder.Clear();
+            MessageBox.Show(ex.Message);
+        }
+    }
+
+    private void EndButton_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            DateTimeOffset now = DateTimeOffset.Now;
+
+            _eventNotificationBuilder.End(now);
+
+            EventNotification eventNotification = _eventNotificationBuilder.Build();
+            string bandNames = eventNotification.BandNames.Count <= 0
+                ? "Empty"
+                : eventNotification.BandNames.Aggregate((x, y) => x + "," + y);
+            string message =
+$@"{eventNotification.StartDateTime}
+{eventNotification.EndDateTime}
+{eventNotification.Name}
+{bandNames}";
+
+            MessageBox.Show(message);
+
+            _eventNotificationBuilder.Clear();
+        }
+        catch (Exception ex)
+        {
+            _eventNotificationBuilder.Clear();
+            MessageBox.Show(ex.Message);
+        }
     }
 }
