d5985b172b7ccffe4d1ffb3d8e924757737dee61
higege-amdeker <higezozozo@gmail.com>
2023-09-12 22:05:00 +0900

    Band スケルトンクラスを追加

---
+++ src/HigeDaruma.DemoDevidedWpf.AppCore/LiveEventModels/Band.cs
@@ -0,0 +1,11 @@
+﻿namespace HigeDaruma.DemoDevidedWpf.AppCore.LiveEventModels;
+
+/// <summary>
+/// バンドを表現する DomainObject です
+/// </summary>
+public sealed class Band
+{
+    public Band()
+    {
+    }
+}
---
+++ tests/HigeDaruma.DemoDevidedWpf.AppCore.UnitTest/LiveEventModels/BandTest.cs
@@ -0,0 +1,19 @@
+﻿using HigeDaruma.DemoDevidedWpf.AppCore.LiveEventModels;
+
+namespace HigeDaruma.DemoDevidedWpf.AppCore.UnitTest.LiveEventModels;
+
+/// <summary>
+/// <see cref="Band"/> の UnitTest です
+/// </summary>
+public sealed class BandTest
+{
+    // Ctor()
+
+    [Fact]
+    public void Ctor_ReturnsOk()
+    {
+        Band result = new();
+
+        Assert.NotNull(result);
+    }
+}
